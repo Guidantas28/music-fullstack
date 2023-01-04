77830c4ac1d16efcec44c1a74aba54ddb51f3722
Guilherme Dantas <guilhermedantas788@gmail.com>
2023-01-04 19:20:27 -0300

    finished the index pahe layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,56 @@
+import { Box, Text, Flex } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
+import prisma from '../lib/prisma'
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       image="https://avatars.githubusercontent.com/u/73999334?v=4"
       roundImage
-      color="pink"
+      color="gray"
       subtitle="profile"
       title="Guilherme Dantas"
       description="3 public playlists"
     >
-      <div>Home Page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md" color="whiteAlpha.700">
+            only visible to you
+          </Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="17%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large" fontWeight="semibold">
+                    {artist.name}
+                  </Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   )
+}
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({})
+
+  return {
+    props: { artists },
+  }
 }
 
 export default Home
