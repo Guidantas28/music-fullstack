48664a4b3d0e865d37fa09bacd7860c634e10fd4
Guilherme Dantas <guilhermedantas788@gmail.com>
2023-01-04 16:54:50 -0300

    profile page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from '../components/gradientLayout'
+
 const Home = () => {
-  return <div>home</div>
+  return (
+    <GradientLayout
+      image="https://avatars.githubusercontent.com/u/73999334?v=4"
+      roundImage
+      color="pink"
+      subtitle="profile"
+      title="Guilherme Dantas"
+      description="3 public playlists"
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  )
 }
 
 export default Home
